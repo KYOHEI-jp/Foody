3f2c67cbba6e49907aca709022d892e5f202b149
渡辺恭平 <kingdomhearts510@gmail.com>
2021-05-09 10:24:01 +0900

    bottomNav

--- app/src/main/java/com/example/foody/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/example/foody/FavoriteRecipesFragment.kt
@@ -13,6 +13,6 @@ class FavoriteRecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faborite_recipes, container, false)
+        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
     }
 }
